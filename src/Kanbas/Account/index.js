498cd3ecd28ffdb6868f1d5e40254d5decdbb169
kevinli1206 <li.kevin1@northeastern.edu>
2023-10-03 17:14:22 -0400

    initial a3

---
+++ src/Kanbas/Account/index.js
@@ -0,0 +1,4 @@
+function Account() {
+
+}
+export default Account;
---
+++ src/Labs/a3/index.js
@@ -0,0 +1,19 @@
+import Classes from "./Classes";
+import Styles from "./Styles";
+import JavaScript from "./JavaScript";
+import ConditionalOutput from "./ConditionalOutput";
+import ToDoList from "./todo/TodoList";
+
+function Assignment3() {
+    return (
+        <div>
+            <h1>Assignment 3</h1>
+            <ToDoList/>
+            <ConditionalOutput />
+            <Styles />
+            <Classes />
+            <JavaScript />
+        </div>
+    );
+}
+export default Assignment3;
